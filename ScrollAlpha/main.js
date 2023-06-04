360bdd176f9f1ef4124e22be30f5cb8df5de91c6
Dani Kurniawan <47886545+dkzhen@users.noreply.github.com>
2023-06-05 00:08:48 +0700

    Update main.js

--- ScrollAlpha/main.js
+++ ScrollAlpha/main.js
@@ -1,15 +1,6 @@
 const FaucetV1 = require("./syncswap/FaucetV1");
 const axios = require("axios")
-async function main() {
-  try {
-    await FaucetV1();
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
 
-main();
-const FaucetV1 = require("./syncswap/FaucetV1");
 
 const webhookUrl =
   "https://discord.com/api/webhooks/1110934381872300103/uONGg3CK-bpRrr_WK4GOX75CsLtqQv7jzfG2N4x5BXYD--w4_UY3UgqJY3AC0vQGyxqP";
