1230e6fcba837242727c6550690aaf5dd921162d
dkzhen <dani314123@gmail.com>
2023-06-04 22:30:16 +0700

    scroll usdc

--- ScrollAlpha/syncswap/FaucetV1.js
+++ ScrollAlpha/syncswap/FaucetV1.js
@@ -2,7 +2,7 @@ const transferFee = require("./utils/BatchSendETH");
 const claimAllTokens = require("./utils/ClaimUSDC");
 
 async function FaucetV1() {
-  //await transferFee();
+  await transferFee();
   await claimAllTokens();
 }
 module.exports = FaucetV1;
