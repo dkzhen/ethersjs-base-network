376d45c2f76ec601162e88b285d97092e7269887
dkzhen <dani314123@gmail.com>
2023-06-08 23:49:07 +0700

    loop faucet

--- GeneratePrivateKey.js
+++ GeneratePrivateKey.js
@@ -1,5 +1,6 @@
 const { ethers } = require("ethers");
 const fs = require("fs");
+const path = require("path");
 const config = require("./config");
 
 async function generateAccounts(numAccounts) {
@@ -19,11 +20,33 @@ async function generateAccounts(numAccounts) {
 }
 
 async function generateAndSaveAccounts(numAccounts, fileName) {
+  let targetFileName = fileName;
+  let counter = 1;
+
+  while (fs.existsSync(targetFileName)) {
+    const extension = path.extname(fileName);
+    const baseName = path.basename(fileName, extension);
+    const randomChar = generateRandomString(counter);
+    targetFileName = `${baseName}${randomChar}${extension}`;
+    counter++;
+  }
+  function generateRandomString(length) {
+    let result = "";
+    const characters =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+
+    return result;
+  }
   const accounts = await generateAccounts(numAccounts);
 
   const jsonData = JSON.stringify(accounts, null, 2);
 
-  fs.writeFileSync(fileName, jsonData);
+  fs.writeFileSync(targetFileName, jsonData);
 
   console.log(
     `Generated ${numAccounts} accounts and saved them to ${fileName}.`
